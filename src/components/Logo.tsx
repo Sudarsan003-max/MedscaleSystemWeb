87dee9ebc7ee3c779eefd34bd6c60e8cbb66673c
SUDARSANANARAYANAN U R <sudarsananarayanan003@gmail.com>
2026-06-18 19:15:04 +0530

    style: fix logo SVG dimensions and spacing to perfectly match side-by-side comparison

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,22 +6,22 @@ interface LogoProps {
 export default function Logo({ className = "h-6 w-6", color = "#1b3bf5" }: LogoProps) {
   return (
     <svg
-      viewBox="80 80 660 390"
+      viewBox="0 -20 590 420"
       className={className}
       fill={color}
       xmlns="http://www.w3.org/2000/svg"
     >
       {/* Left Column (Thick) */}
-      <polygon points="100,450 100,100 180,180 180,450" />
+      <polygon points="20,380 20,20 100,100 100,380" />
 
       {/* Left Column (Thin) */}
-      <polygon points="210,450 210,210 240,240 240,450" />
+      <polygon points="120,380 120,120 160,160 160,380" />
 
       {/* Right Column */}
-      <polygon points="540,450 540,270 620,190 620,450" />
+      <polygon points="420,380 420,160 500,80 500,380" />
 
       {/* Middle V-Shape and Arrow (Unified path to avoid seams) */}
-      <polygon points="270,300 420,450 675,195 690,210 720,120 630,150 645,165 420,390 270,240" />
+      <polygon points="180,180 300,300 525,75 540,90 570,0 480,30 495,45 300,240 180,120" />
     </svg>
   );
 }
