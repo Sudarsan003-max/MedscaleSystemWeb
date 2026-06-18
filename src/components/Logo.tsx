2d105d8148cec180dc4cf28d4c3a04585ce2104b
SUDARSANANARAYANAN U R <sudarsananarayanan003@gmail.com>
2026-06-18 19:34:03 +0530

    style: flatten logo SVG slope to 37 degrees for exact aspect ratio matching

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,22 +6,22 @@ interface LogoProps {
 export default function Logo({ className = "h-6 w-6", color = "#1b3bf5" }: LogoProps) {
   return (
     <svg
-      viewBox="0 -100 670 500"
+      viewBox="0 -60 960 460"
       className={className}
       fill={color}
       xmlns="http://www.w3.org/2000/svg"
     >
       {/* Left Column (Thick) */}
-      <polygon points="20,380 20,20 100,100 100,380" />
+      <polygon points="40,380 40,50 160,140 160,380" />
 
       {/* Left Column (Thin) */}
-      <polygon points="120,380 120,120 160,160 160,380" />
+      <polygon points="200,380 200,170 260,215 260,380" />
 
       {/* Right Column */}
-      <polygon points="540,380 540,80 620,0 620,380" />
+      <polygon points="800,380 800,110 920,20 920,380" />
 
       {/* Middle V-Shape and Arrow (Unified path to avoid seams) */}
-      <polygon points="180,180 300,300 605,-5 620,10 650,-80 560,-50 575,-35 300,240 180,120" />
+      <polygon points="292,239 440,350 820,65 840,95 880,-25 760,-25 780,5 440,260 292,149" />
     </svg>
   );
 }
