283fd316bb865ce69740662b7e2a8150309905e3
SUDARSANANARAYANAN U R <sudarsananarayanan003@gmail.com>
2026-06-18 19:11:34 +0530

    style: restore uniform vertical columns and correct wedge gaps in logo SVG

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps {
 export default function Logo({ className = "h-6 w-6", color = "#1b3bf5" }: LogoProps) {
   return (
     <svg
-      viewBox="80 80 720 390"
+      viewBox="80 80 660 390"
       className={className}
       fill={color}
       xmlns="http://www.w3.org/2000/svg"
@@ -14,14 +14,14 @@ export default function Logo({ className = "h-6 w-6", color = "#1b3bf5" }: LogoP
       {/* Left Column (Thick) */}
       <polygon points="100,450 100,100 180,180 180,450" />
 
-      {/* Left Column (Thin Wedge/Trapezoid) */}
-      <polygon points="215,450 215,215 450,450" />
+      {/* Left Column (Thin) */}
+      <polygon points="210,450 210,210 240,240 240,450" />
 
       {/* Right Column */}
-      <polygon points="620,450 620,260 700,180 700,450" />
+      <polygon points="540,450 540,270 620,190 620,450" />
 
       {/* Middle V-Shape and Arrow (Unified path to avoid seams) */}
-      <polygon points="320,280 480,440 740,180 760,200 780,100 680,120 700,140 480,360 320,200" />
+      <polygon points="270,300 420,450 675,195 690,210 720,120 630,150 645,165 420,390 270,240" />
     </svg>
   );
 }
