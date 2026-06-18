c3a4b094bbcb8be6ac5e1163288659ce03a8f309
SUDARSANANARAYANAN U R <sudarsananarayanan003@gmail.com>
2026-06-18 19:40:49 +0530

    style: finalize exact double-wedge SVG logo coordinates

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps {
 export default function Logo({ className = "h-6 w-6", color = "#1b3bf5" }: LogoProps) {
   return (
     <svg
-      viewBox="0 -60 960 460"
+      viewBox="0 -20 960 420"
       className={className}
       fill={color}
       xmlns="http://www.w3.org/2000/svg"
@@ -14,14 +14,14 @@ export default function Logo({ className = "h-6 w-6", color = "#1b3bf5" }: LogoP
       {/* Left Column (Thick) */}
       <polygon points="40,380 40,50 160,140 160,380" />
 
-      {/* Left Column (Thin) */}
-      <polygon points="200,380 200,170 260,215 260,380" />
+      {/* Left Column (Thin Wedge/Trapezoid) */}
+      <polygon points="200,380 200,170 480,380" />
 
-      {/* Right Column */}
-      <polygon points="800,380 800,110 920,20 920,380" />
+      {/* Right Column (Wedge/Trapezoid) */}
+      <polygon points="520,380 920,80 920,380" />
 
       {/* Middle V-Shape and Arrow (Unified path to avoid seams) */}
-      <polygon points="292,239 440,350 820,65 840,95 880,-25 760,-25 780,5 440,260 292,149" />
+      <polygon points="292,239 440,350 820,95 840,125 880,5 760,5 780,35 440,260 292,149" />
     </svg>
   );
 }
